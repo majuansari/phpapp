571a1dffcf40d597d1a195bb33c16477126a040a
Maju Ansari <maju.ansari@landmarkgroup.in>
2020-05-24 13:48:35 +0530

    first commit

---
+++ docker/src/index.php
@@ -0,0 +1,7 @@
+<?php
+echo "try 7 <br>";
+echo "Hello, World sit! <br>";
+echo "Hello Jenkins X! v6 <br>";
+echo '<img border="0" src="https://jenkins-x.io/images/logo.png" width="300" height="300" />';
+
+?>
