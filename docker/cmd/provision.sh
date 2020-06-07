bb6e6eb7632c8662fdda2d152cbbc1931912da1b
Maju Ansari <maju.ansari@landmarkgroup.in>
2020-06-07 18:28:05 +0530

    fixes

--- docker/cmd/provision.sh
+++ docker/cmd/provision.sh
@@ -1,16 +1,16 @@
 cd /code
 echo "Laravel provisioning..."
 
-dirs=( "/storage/public" "/storage/framework/cache" "/storage/framework/sessions" "/storage/framework/testing" "/storage/framework/views" "/storage/logs" )
+dirs=( "/code/storage/public" "/code/storage/framework/cache" "/code/storage/framework/sessions" "/code/storage/framework/testing" "/code/storage/framework/views" "/code/storage/logs" )
 
 for i in "${dirs[@]}"; do
     [ -d "$i" ] || mkdir -p $i
 done
 
-chmod -R 777 /storage
-chmod -R 777 /public/storage
-chown -R 9000:9000 /storage
-chown -R 9000:9000 /public/storage
+chmod -R 777 /code/storage
+chmod -R 777 /code/public/storage
+chown -R 9000:9000 /code/storage
+chown -R 9000:9000 /code/public/storage
 
 # move this to a different container
 composer install 
