6b90308cc52094ff4739e195d7b2b98ac44466f1
Maju Ansari <maju.ansari@landmarkgroup.in>
2020-06-07 20:12:20 +0530

    fixes

--- docker/cmd/provision.sh
+++ docker/cmd/provision.sh
@@ -1,23 +1,25 @@
+#!/bin/sh
 cd /code
 echo "Laravel provisioning..."
 
-dirs=( "/code/public/storage" "/code/storage/public" "/code/storage/framework/cache" "/code/storage/framework/sessions" "/code/storage/framework/testing" "/code/storage/framework/views" "/code/storage/logs" )
+sleep 10
+# dirs=( "/code/public/storage" "/code/storage/public" "/code/storage/framework/cache" "/code/storage/framework/sessions" "/code/storage/framework/testing" "/code/storage/framework/views" "/code/storage/logs" )
 
-for i in "${dirs[@]}"; do
-    [ -d "$i" ] || mkdir -p $i
-done
+# for i in "${dirs[@]}"; do
+#     [ -d "$i" ] || mkdir -p $i
+# done
 
-chmod -R 777 /code/storage
-chmod -R 777 /code/public/storage
-chown -R 9000:9000 /code/storage
-chown -R 9000:9000 /code/public/storage
+# chmod -R 777 /code/storage
+# chmod -R 777 /code/public/storage
+# chown -R 9000:9000 /code/storage
+# chown -R 9000:9000 /code/public/storage
 
-# move this to a different container
-composer install 
+# # move this to a different container
+# composer install 
 
-php artisan migrate --force
-php artisan clear-compiled
-php artisan cache:clear
-php artisan view:clear
-php artisan route:clear
-php artisan config:clear
+# php artisan migrate --force
+# php artisan clear-compiled
+# php artisan cache:clear
+# php artisan view:clear
+# php artisan route:clear
+# php artisan config:clear
