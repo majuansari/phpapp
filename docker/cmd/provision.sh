c5277a2a0c41b3d9644900d4e0757f66d2c344a5
Maju Ansari <maju.ansari@landmarkgroup.in>
2020-06-07 22:58:46 +0530

    fixes

--- docker/cmd/provision.sh
+++ docker/cmd/provision.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
-cd /codetemp
-echo "Laravel provisioning..."
-
-dirs=( "/codetemp/public/storage" "/codetemp/storage/public" "/codetemp/storage/framework/cache" "/codetemp/storage/framework/sessions" "/codetemp/storage/framework/testing" "/codetemp/storage/framework/views" "/codetemp/storage/logs" )
-
-for i in "${dirs[@]}"; do
-    [ -d "$i" ] || mkdir -p $i
-done
-
-chmod -R 777 /codetemp/storage
-chmod -R 777 /codetemp/public/storage
-chown -R 9000:9000 /codetemp/storage
-chown -R 9000:9000 /codetemp/public/storage
+cd /codetemp || exit;
+echo "Laravel provisioning...";
 
 # # move this to a different container
 composer install
