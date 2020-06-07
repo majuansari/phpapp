29ea969ad02ae12f2d8b39a0afeac5598c81cb11
Maju Ansari <maju.ansari@landmarkgroup.in>
2020-06-07 17:17:42 +0530

    changes + init containers

---
+++ docker/cmd/provision.sh
@@ -0,0 +1,23 @@
+cd /code
+echo "Laravel provisioning..."
+
+dirs=( "/code/storage/code/public" "/code/storage/framework/cache" "/code/storage/framework/sessions" "/code/storage/framework/testing" "/code/storage/framework/views" "/code/storage/logs" )
+
+for i in "${dirs[@]}"; do
+    [ -d "$i" ] || mkdir -p $i
+done
+
+chmod -R 777 /code/storage
+chmod -R 777 /code/public/storage
+chown -R 9000:9000 /code/storage
+chown -R 9000:9000 /code/public/storage
+
+# move this to a different container
+composer install 
+
+php artisan migrate --force
+php artisan clear-compiled
+php artisan cache:clear
+php artisan view:clear
+php artisan route:clear
+php artisan config:clear
