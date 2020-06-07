c2f08bcc5cd22da58bd4c4e7d873e295b87f2217
Maju Ansari <maju.ansari@landmarkgroup.in>
2020-06-07 22:55:28 +0530

    fixes

--- docker/cmd/provision.sh
+++ docker/cmd/provision.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-cd /code
+cd /codetemp
 echo "Laravel provisioning..."
 
-dirs=( "/code/public/storage" "/code/storage/public" "/code/storage/framework/cache" "/code/storage/framework/sessions" "/code/storage/framework/testing" "/code/storage/framework/views" "/code/storage/logs" )
+dirs=( "/codetemp/public/storage" "/codetemp/storage/public" "/codetemp/storage/framework/cache" "/codetemp/storage/framework/sessions" "/codetemp/storage/framework/testing" "/codetemp/storage/framework/views" "/codetemp/storage/logs" )
 
 for i in "${dirs[@]}"; do
     [ -d "$i" ] || mkdir -p $i
 done
 
-chmod -R 777 /code/storage
-chmod -R 777 /code/public/storage
-chown -R 9000:9000 /code/storage
-chown -R 9000:9000 /code/public/storage
+chmod -R 777 /codetemp/storage
+chmod -R 777 /codetemp/public/storage
+chown -R 9000:9000 /codetemp/storage
+chown -R 9000:9000 /codetemp/public/storage
 
 # # move this to a different container
-composer install -d=/code
+composer install
 
 php artisan migrate --force
 php artisan clear-compiled
