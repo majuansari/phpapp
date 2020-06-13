9ba6480149e5cfb0dac700b740172fe9fc3b6359
Maju Ansari <maju.ansari@landmarkgroup.in>
2020-06-13 13:22:31 +0530

    fixes

--- docker/cmd/provision.sh
+++ docker/cmd/provision.sh
@@ -10,10 +10,11 @@ do
 done
 
 
-chmod -R 777 /pre-code/storage
-chmod -R 777 /pre-code/public/storage
+chown -R 9000:9000 /pre-code
 chown -R 9000:9000 /pre-code/storage
 chown -R 9000:9000 /pre-code/public/storage
+chmod -R 777 /pre-code/storage
+chmod -R 777 /pre-code/public/storage
 
 ls -la /pre-code/storage/
 # # move this to a different container
