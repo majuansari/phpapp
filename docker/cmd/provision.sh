b4935760e1d0795f459746c7c1bc883ba9629d6b
Maju Ansari <maju.ansari@landmarkgroup.in>
2020-06-13 10:44:11 +0530

    fixes

--- docker/cmd/provision.sh
+++ docker/cmd/provision.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-cd /codetemp || exit;
+cd /pre-code || exit;
 echo "Laravel provisioning...";
 
 
-dirs=("/codetemp/public/storage" "/codetemp/storage/public" "/codetemp/storage/framework/cache" "/codetemp/storage/framework/sessions" "/codetemp/storage/framework/testing" "/codetemp/storage/framework/views" "/codetemp/storage/logs" )
+dirs=("/pre-code/public/storage" "/pre-code/storage/public" "/pre-code/storage/framework/cache" "/pre-code/storage/framework/sessions" "/pre-code/storage/framework/testing" "/pre-code/storage/framework/views" "/pre-code/storage/logs" )
 for d in "${dirs[@]}"
 do
     [ -d "$d" ] || mkdir -p $d
 done
 
 
-chmod -R 777 /codetemp/storage
-chmod -R 777 /codetemp/public/storage
-chown -R www-data:www-data /codetemp/storage
-chown -R www-data:www-data /codetemp/public/storage
+chmod -R 777 /pre-code/storage
+chmod -R 777 /pre-code/public/storage
+chown -R www-data:www-data /pre-code/storage
+chown -R www-data:www-data /pre-code/public/storage
 # # move this to a different container
 composer install
 
@@ -24,3 +24,4 @@ php artisan view:clear
 php artisan route:clear
 php artisan config:clear
 
+cp -rp /pre-code/ /codetemp/
